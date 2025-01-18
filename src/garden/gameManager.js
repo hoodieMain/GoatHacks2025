6e9a78b99f194190c44a322a3b264aa29e0082ba
Keith Miller <kmiller4@wpi.edu>
2025-01-18 17:15:25 -0500

    Added a basic XP tracker to the main page - to be styled.

--- src/garden/gameManager.js
+++ src/garden/gameManager.js
@@ -3,12 +3,14 @@ import { Plant } from './plant.js';
 
 class GameManager {
     
-    constructor(plant_pots = 3, plants = [], money = []){
-        this.plant_pots = 3;
-        this.plants = [];
-        this.money = 0;
+    constructor(plant_pots = 3, plants = [], money = [], xp = 0){
+        this.plant_pots = plant_pots;
+        this.plants = plants;
+        this.money = money;
+        this._xp = xp;
     }
 
+    // Adds a new plant to the game manager.
     newPlant(new_plant) {
         if (this.plants.length < this.plant_pots){
             this.plants.push(new_plant);
@@ -17,13 +19,31 @@ class GameManager {
         }
     }
 
+    // Occurs every X seconds that the pomodoro runs. Grows plants and gives XP.
     gameTick() {
         for (let plant of this.plants) {
             this.money += plant.grow(null, true);
         }
         console.log("Money: $" + this.money);
+        this.setXP = this._xp + 10;
+        console.log("XP: " + this._xp);
+        
         console.log();
-    
+        
+    }
+
+    // Getter for XP
+    get getXP() {
+        return this._xp;
+    }
+
+    // Setter for XP
+    set setXP(value) {
+        this._xp = value;
+
+        // Emit a custom event when XP changes
+        const event = new CustomEvent('xpChanged', { detail: { xp: this._xp } });
+        document.dispatchEvent(event); // Notify listeners
     }
 } 
 
